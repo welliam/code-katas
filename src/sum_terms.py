ddf1ae630251614e61bd404119a191a5eb519054
Justin Lange <well1912@gmail.com>
2016-08-14 14:16:48 -0700

    Fix sum_terms docstrings

--- src/sum_terms.py
+++ src/sum_terms.py
@@ -1,4 +1,4 @@
-"""Solves sum of nth terms exercise from code katas.
+"""Solve sum of nth terms exercise from code katas.
 http://www.codewars.com/kata/sum-of-the-first-nth-term-of-series/train/python
 """
 
@@ -6,6 +6,7 @@ from __future__ import division
 
 
 def series_sum(n):
-    """A string of the sum of the first n terms in the series
-    [1, 1/4, 1/7 ...], to two decimal points."""
+    """The sum of the first n terms in the series [1, 1/4, 1/7 ...],
+    to two decimal points. Return a string
+    """
     return '{0:.2f}'.format(sum(1/(1 + i*3) for i in range(n)))

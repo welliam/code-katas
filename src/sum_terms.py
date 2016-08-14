6b3c2f19d517c02bd80e51039bcd266e8c289cb4
Justin Lange <well1912@gmail.com>
2016-08-14 14:14:29 -0700

    Add docstrings to sum_terms.py

--- src/sum_terms.py
+++ src/sum_terms.py
@@ -1,4 +1,11 @@
+"""Solves sum of nth terms exercise from code katas.
+http://www.codewars.com/kata/sum-of-the-first-nth-term-of-series/train/python
+"""
+
 from __future__ import division
 
+
 def series_sum(n):
+    """A string of the sum of the first n terms in the series
+    [1, 1/4, 1/7 ...], to two decimal points."""
     return '{0:.2f}'.format(sum(1/(1 + i*3) for i in range(n)))

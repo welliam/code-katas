bb073b9b3b19f8ac06acb4567b283f0d5a1a24d0
Justin Lange <well1912@gmail.com>
2016-08-14 13:47:38 -0700

    Add setup.py and tox.ini

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Package implements a mailroom helper function."""
+from setuptools import setup
+
+setup(
+    name='Code Katas',
+    description='My Codewars solutions.',
+    version='0.1.0',
+    author='Justin Lange',
+    author_email='well1912@gmail.com',
+    install_requires=[],
+    extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
+)

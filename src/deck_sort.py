22f3e4079c171c84a6222d2833e63fa58df68cff
Justin Lange <well1912@gmail.com>
2016-08-28 19:16:49 -0700

    Add docstrings

--- src/deck_sort.py
+++ src/deck_sort.py
@@ -5,6 +5,9 @@ card_ord = 'A23456789TJQK'
 
 
 def deck_sort(t):
+    """Sort cards using priority queue.
+
+    Ace = A, 10=T, Joker=J, Queen=Q, King=K."""
     pq = PriorityQueue()
     for c in t:
         pq.insert(c, card_ord.find(c))
@@ -15,4 +18,7 @@ def deck_sort(t):
 
 
 def deck_sort2(t):
+    """Sort as if input were a deck of cards
+
+    Ace = A, 10=T, Joker=J, Queen=Q, King=K."""
     return list(sorted(t, key=card_ord.find))

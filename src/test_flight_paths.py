187c1c7aff8ba65ca98b42eae2df54666a1c1c0d
Justin Lange <well1912@gmail.com>
2016-09-15 21:14:42 -0700

    Fix flight paths tests

--- src/test_flight_paths.py
+++ src/test_flight_paths.py
@@ -1,12 +1,12 @@
-"""Test filght_paths.py."""
+"""Test flight_paths.py."""
 
 import pytest
 
 
-CITY_KEYS = ['city', 'destination_city', 'lat_lon']
+CITY_KEYS = ['city', 'destination_cities', 'lat_lon']
 
 
 @pytest.mark.parametrize('key', CITY_KEYS)
 def test_flight_paths_have_keys(key):
     from flight_paths import get_flight_paths
-    assert all(map(lambda c: key in get_flight_paths()))
+    assert all(map(lambda c: key in c, get_flight_paths()))

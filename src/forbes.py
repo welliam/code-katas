89ee50fb364e335c372d8e93138046fbb8d999ca
Justin Lange <well1912@gmail.com>
2016-09-18 23:08:10 -0700

    Test and implement oldest_under_80

--- src/forbes.py
+++ src/forbes.py
@@ -7,3 +7,11 @@ FORBES_FILE = 'static/forbes_billionaires_2016.json'
 def load_forbes(filename=FORBES_FILE):
     """Load forbes json information."""
     return json.load(open(filename))
+
+
+def oldest_under_80(billionares):
+    """Returns the oldest member of billionares under 80."""
+    oldest = None
+    for b in filter(lambda b: b['age'] < 80, billionares):
+        oldest = b if (not oldest or b['age'] > oldest['age']) else oldest
+    return oldest
--- src/test_forbes.py
+++ src/test_forbes.py
@@ -1,4 +1,24 @@
+import pytest
+
+
+BILLIONAIRES = [[
+    {'age': 80, 'name': 'correct'},
+    {'age': 27, 'name': 'incorrect'},
+    {'age': 0, 'name': 'incorrect'},
+    {'age': 81, 'name': 'incorrect'}
+], [
+    {'age': 1, 'name': 'correct'},
+    {'age': 81, 'name': 'incorrect'}
+]]
+
+
 def test_load_forbes():
     """Test forbes loading function."""
     from .forbes import load_forbes, FORBES_FILE
     assert isinstance(load_forbes(FORBES_FILE), list)
+
+
+@pytest.mark.parametrize('billionaires', BILLIONAIRES)
+def test_oldest_under_80(billionaires):
+    from .forbes import oldest_under_80
+    assert oldest_under_80(billionaires)

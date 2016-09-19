1c4796e60c6b3b372e89e7e1573071b7e262c4e8
Justin Lange <well1912@gmail.com>
2016-09-18 23:30:03 -0700

    Fix tests, add docstrings, add test_youngest

--- src/test_forbes.py
+++ src/test_forbes.py
@@ -2,7 +2,7 @@ import pytest
 
 
 BILLIONAIRES = [[
-    {'age': 80, 'name': 'correct'},
+    {'age': 79, 'name': 'correct'},
     {'age': 27, 'name': 'incorrect'},
     {'age': 0, 'name': 'incorrect'},
     {'age': 81, 'name': 'incorrect'}
@@ -20,5 +20,17 @@ def test_load_forbes():
 
 @pytest.mark.parametrize('billionaires', BILLIONAIRES)
 def test_oldest_under_80(billionaires):
+    """Test youngest returns oldest billionaire under 80."""
     from .forbes import oldest_under_80
-    assert oldest_under_80(billionaires)
+    assert oldest_under_80(billionaires)['name'] == 'correct'
+
+
+def test_youngest():
+    """Test youngest returns youngest billionaire with valid age."""
+    from .forbes import youngest
+    billionaires = [
+        {'age': 1, 'name': 'correct'},
+        {'age': -1, 'name': 'incorrect'},
+        {'age': 2, 'name': 'incorrect'}
+    ]
+    assert youngest(billionaires)['name'] == 'correct'
